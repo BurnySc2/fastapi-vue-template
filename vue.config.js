70fd8000bc46c8fc584a367e04ea247b2f123ba4
BuRny <gamingburny@gmail.com>
2020-12-31 20:08:06 +0100

    Fix publicPath again

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '',
+  publicPath: '',
   devServer: {
     proxy: "http://0.0.0.0:8000",
   }
