90c8ab8df527371f33cab1e4bc86adff43de3bf3
BuRny <gamingburny@gmail.com>
2020-12-31 19:48:07 +0100

    Set indention to 2 spaces

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="flex flex-row">
-        <button class="m-1 p-2 border-2" v-on:click="removeTodo(todo.id)">Remove</button>
-        <div class="m-1 p-1">{{ todo.content }}</div>
-    </div>
+  <div class="flex flex-row">
+    <button class="m-1 p-2 border-2" v-on:click="removeTodo(todo.id)">Remove</button>
+    <div class="m-1 p-1">{{ todo.content }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Card",
-    props: {
-        todo: Object,
-        removeTodo: Function,
-    },
+  name: "Card",
+  props: {
+    todo: Object,
+    removeTodo: Function,
+  },
 }
 </script>
 
